ec509798d0fe97e64d4a8da792ef9298f2a9edd9
Abhishek Singh <abhisheks71@yahoo.com>
2026-01-31 02:07:38 +0530

    update overall color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          orange: '#ff7a2d',
+          cyan: '#19d3ff',
+          panel: '#0f0f10',
+          surface: '#111015',
+          accent: '#ffd166'
+        }
+      },
+      fontFamily: {
+        heading: ['Rajdhani', 'sans-serif'],
+        body: ['Inter', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
